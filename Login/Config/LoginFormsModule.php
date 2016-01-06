1a52eb149d5cb020f5eb386f15a117e6f42af9d4
Cláudio Silva <claudio.silva@impactwave.com>
2016-01-06 15:39:44 +0000

    First commit.

---
+++ Login/Config/LoginFormsModule.php
@@ -0,0 +1,57 @@
+<?php
+namespace Selenia\Plugins\Login\Config;
+
+use Selenia\Plugins\Login\Controllers\Login;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Selenia\Core\Assembly\Services\ModuleServices;
+use Selenia\Interfaces\Http\RequestHandlerInterface;
+use Selenia\Interfaces\Http\RouterInterface;
+use Selenia\Interfaces\ModuleInterface;
+use Selenia\Interfaces\Navigation\NavigationInterface;
+use Selenia\Interfaces\Navigation\NavigationProviderInterface;
+
+class LoginFormsModule implements ModuleInterface, RequestHandlerInterface, NavigationProviderInterface
+{
+  /** @var RouterInterface */
+  private $router;
+  /** @var LoginFormsSettings */
+  private $settings;
+
+  function __invoke (ServerRequestInterface $request, ResponseInterface $response, callable $next)
+  {
+    return $this->router
+      ->set ([
+        $this->settings->urlPrefix () . '...' => [
+          'login' => Login::class,
+        ],
+      ])
+      ->__invoke ($request, $response, $next);
+  }
+
+  function configure (ModuleServices $module, LoginFormsSettings $settings, RouterInterface $router)
+  {
+    $this->settings = $settings;
+    $this->router   = $router;
+    $module
+      ->provideTranslations ()
+      ->onPostConfig (function () use ($module) {
+        $module
+          ->provideNavigation ($this)
+          ->registerRouter ($this);
+      });
+  }
+
+  function defineNavigation (NavigationInterface $navigation)
+  {
+    $prefix = $this->settings->urlPrefix ();
+    $navigation->add([
+      "$prefix/login" => $navigation
+        ->link()
+        ->id ('login')
+        ->title ('$LOGIN_PROMPT')
+        ->visible (N),
+    ]);
+  }
+
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Login Forms
+
+> A set of pages and transactional emails for user login, signup and password reset.
+
+## License
+
+The Selenia framework is open-source software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
+**Selenia framework** - Copyright &copy; 2015 Impactwave, Lda.
